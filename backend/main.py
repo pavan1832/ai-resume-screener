8d263f2064a42f36fdb19173941b03aeff0ab0ef
Lokpavan P <141185596+pavan1832@users.noreply.github.com>
2026-04-19 11:22:38 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-resume-screener-inky.vercel.app/",  # add your Vercel URL
+        "*",  # or keep this for simplicity
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
